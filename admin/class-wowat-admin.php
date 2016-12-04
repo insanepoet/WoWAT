0a8c5d0516aa2d321a3dc3358061fa227cf746db
Insanepoet <rfinchamsonata@gmail.com>
2016-12-04 10:39:38 -0500

    Update class-wowat-admin.php

--- admin/class-wowat-admin.php
+++ admin/class-wowat-admin.php
@@ -100,4 +100,33 @@ class Wowat_Admin {
 
 	}
 
+ 
+	public function add_plugin_admin_menu() {
+
+	    /*
+	     * Add a settings page for this plugin to the Settings menu.
+	     *
+	     * NOTE:  Alternative menu locations are available via WordPress administration menu functions.
+	     *
+	     *        Administration Menus: http://codex.wordpress.org/Administration_Menus
+	     *
+	     */
+	    add_options_page( 'WoW Api Tools Configuration', 'WoW Api Tools', 'manage_options', $this->plugin_name, array($this, 'display_plugin_setup_page')
+	    );
+	}
+
+	public function add_action_links( $links ) {
+	    /*
+	    *  Documentation : https://codex.wordpress.org/Plugin_API/Filter_Reference/plugin_action_links_(plugin_file_name)
+	    */
+	   $settings_link = array(
+	    '<a href="' . admin_url( 'options-general.php?page=' . $this->plugin_name ) . '">' . __('Settings', $this->plugin_name) . '</a>',
+	   );
+	   return array_merge(  $settings_link, $links );
+
+	}
+ 
+	public function display_plugin_setup_page() {
+	    include_once( 'partials/wp-wowat-admin-display.php' );
+	}
 }
